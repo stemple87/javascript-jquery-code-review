2b805bf51cca0a7625d385ba20d32684ac6925d9
simon <simonctemple@live.com>
2016-01-15 14:22:14 -0800

    fill in branching. updated city information and rearranged images

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,23 @@ $(document).ready(function() {
     var preference = $("select#preference").val();
     var hotcold = $("select#hotcold").val();
 
-    if (age === "old") {
+    if (age === "old" && (color === "blue" || color === "red") && sea === "yes" && preference === "opera" && hotcold === "hot") {
       $("#rome").show();
-
+    } else if (age === "old" && (color === "blue" || color === "green") && (sea === "yes" || sea === "no") && preference === "opera" && hotcold === "cold") {
+      $("#london").show();
+    } else if (age === "average" && sea === "yes") {
+      $("#rio").show();
+    } else if (age === "average" && sea === "no") {
+      $("#peru").show();
+    } else if (age === "young" && hotcold === "hot") {
+      $("#rio").show();
+    } else if (age === "young" && hotcold === "cold") {
+      $("#london").show();
+    } else if (age === "young" && preference === "opera") {
+      $("#paris").show();
+    } else {
+      $("#paris").show();
     }
-
 
 
   event.preventDefault();
