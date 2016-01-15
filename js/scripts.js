4f424b9ae3466c63fa5094aa9e0587c590dd560a
simon <simonctemple@live.com>
2016-01-15 12:11:21 -0800

    add images and text polished layout. Working to fix bug. Page not storing data correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
-  $("form#celebrity").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
-    var travel = $("select#travel").val();
-    var food = $("select#food").val();
-  }
+  $("form#formgroup").submit(function(event) {
+    var age = $("select#age").val();
+    var color = $("select#color").val();
+    var sea = $("select#sea").val();
+    var preference = $("select#preference").val();
+    var hotcold = $("select#hotcold").val();
 
 
-  if (age >= 30 && travel === "italy" && gender === "female" && food === "pizza" || food === "watermelon") {
-    $("#steve").show();
 
-  } else if (age <= 22  && travel === "hongKong") {
-    $("#gilbert").show();
 
-  } else {
-    $("#maggie").show();
+    $("#peru").show();
+
 
 
   event.preventDefault();
